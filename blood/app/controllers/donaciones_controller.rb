6bfcac66ead621727e80e67bc10a7c33f73a7c78
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-20 23:13:54 -0600

    Agrega método para crear una petición de donación

--- blood/app/controllers/donaciones_controller.rb
+++ blood/app/controllers/donaciones_controller.rb
@@ -3,5 +3,15 @@ class DonacionesController < ApplicationController
   end
 
   def create
+    @donacione = Donacione.new(donacione_params)
+    
+    @donacione.save
+    redirect_to @donacione
   end
+  
+  private
+  def donacione_params
+    params.require(:donacione).permit(:donacione)
+  end
+  
 end
