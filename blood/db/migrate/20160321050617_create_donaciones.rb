7311fffaf520e29caf05090b1d4348cf62c980b1
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 10:43:47 -0600

    Revierte el archivo de migración de las donaciones para que las cosas no salgan mal

--- blood/db/migrate/20160321050617_create_donaciones.rb
+++ blood/db/migrate/20160321050617_create_donaciones.rb
@@ -1,10 +1,15 @@
 class CreateDonaciones < ActiveRecord::Migration
   def change
     create_table :donaciones do |t|
+      t.string :nombre
+      t.integer :telefono
+      t.string :correo
       t.string :nombre_hospital
       t.text :direccion_hospital
       t.integer :tel_hospital
-      t.integer :donadores      
+      t.string :sangre
+      t.integer :donadores
+
       t.timestamps
     end
   end
