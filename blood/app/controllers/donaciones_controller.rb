901919c88d1f582813d5804a408302aa43a7d648
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-20 23:26:22 -0600

    Corrige error en el controlador de donaciones (lo tenía de manera que no permitía al usuario poner ningún parámetro XD)

--- blood/app/controllers/donaciones_controller.rb
+++ blood/app/controllers/donaciones_controller.rb
@@ -15,7 +15,7 @@ class DonacionesController < ApplicationController
   
   private
   def donacione_params
-    params.require(:donacione).permit(:donacione)
+    params.require(:donacione).permit(:nombre, :telefono, :correo, :nombre_hospital, :direccion_hospital, :tel_hospital, :sangre, :donadores)
   end
   
 end
