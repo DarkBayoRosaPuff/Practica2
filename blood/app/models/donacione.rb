2f7c575a43144fad1ae73add9eb99f42ff85454a
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 01:09:35 -0600

    Agrega validación para que el teléfono sea entero y el tipo de sangre, válido

--- blood/app/models/donacione.rb
+++ blood/app/models/donacione.rb
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 class Donacione < ActiveRecord::Base
   validates :nombre, presence: true
-  validates :telefono, presence: true
+  validates :telefono, presence: true, numericality: { only_integer: true }
   validates :correo, presence: true
   validates :nombre_hospital, presence: true
-  validates :sangre, presence: true
+  validates :sangre, presence: true, inclusion: { in: %w(O-, O+, A-, A+, B-, B+, AB-, AB+), message: "%{value} no es un tipo de sangre válido. Los tipos de sangre válidos son: O-, O+, A-, A+, B-, B+, AB- y AB+" }
   
 end
