d54db2a99e770473cbbfc19ecdb34d8ff2318007
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 09:38:49 -0600

    Cambia el archivo de migración de las donaciones para que cada donación tenga un usuario asociado (espero que esto funcione)

--- blood/db/migrate/20160321050617_create_donaciones.rb
+++ blood/db/migrate/20160321050617_create_donaciones.rb
@@ -1,14 +1,11 @@
 class CreateDonaciones < ActiveRecord::Migration
   def change
     create_table :donaciones do |t|
-      t.string :nombre
-      t.integer :telefono
-      t.string :correo
       t.string :nombre_hospital
       t.text :direccion_hospital
       t.integer :tel_hospital
-      t.string :sangre
       t.integer :donadores
+      t.references :user
 
       t.timestamps
     end
