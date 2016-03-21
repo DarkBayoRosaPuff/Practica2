2ab696bb339a43dabfcc43880eeecc84891f5f81
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 10:45:28 -0600

    Modifica el modelo de donaciones, quitándole la información extra que tenía

--- blood/app/models/donacione.rb
+++ blood/app/models/donacione.rb
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 class Donacione < ActiveRecord::Base
   belongs_to :user
-  validates :nombre, presence: true
-  validates :telefono, presence: true, numericality: { only_integer: true }
-  validates :correo, presence: true
   validates :nombre_hospital, presence: true
-  validates :sangre, presence: true, inclusion: { in: %w(O-, O+, A-, A+, B-, B+, AB-, AB+), message: "%{value} no es un tipo de sangre válido. Los tipos de sangre válidos son: O-, O+, A-, A+, B-, B+, AB- y AB+" }
-  
 end
