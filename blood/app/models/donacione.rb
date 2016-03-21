17b66b322c13a451accfcb5f5d1719a1bd92ff4a
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 00:54:00 -0600

    Agrega validación al modelo de donaciones

--- blood/app/models/donacione.rb
+++ blood/app/models/donacione.rb
@@ -1,2 +1,8 @@
 class Donacione < ActiveRecord::Base
+  validates :nombre, presence: true
+  validates :telefono, presence: true
+  validates :correo, presence: true
+  validates :nombre_hospital, presence: true
+  validates :sangre, presence: true
+  
 end
