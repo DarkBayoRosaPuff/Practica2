b1309c253022ef397c373cd989cf7c587641b0f2
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 12:05:14 -0600

    Agrega campo para que al eliminar a un usuario se eliminen sus publicaciones

--- blood/app/models/user.rb
+++ blood/app/models/user.rb
@@ -2,7 +2,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :donaciones
+  has_many :donaciones, dependent: :destroy
   validates :nombre, presence: true
   validates :telefono, presence: true, numericality: { only_integer: true }
   validates :sangre, presence: true, inclusion: { in: %w(O-, O+, A-, A+, B-, B+, AB-, AB+), message: "%{value} no es un tipo de sangre válido. Los tipos de sangre válidos son: O-, O+, A-, A+, B-, B+, AB- y AB+" }
