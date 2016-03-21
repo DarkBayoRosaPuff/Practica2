d6fc5162b97b64802cb21d9729643fc850712d6a
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-03-21 10:52:35 -0600

    Agrega verificación para que todo usuario tenga la información necesaria

--- blood/app/models/user.rb
+++ blood/app/models/user.rb
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :donaciones
+  validates :nombre, presence: true
+  validates :telefono, presence: true, numericality: { only_integer: true }
+  validates :sangre, presence: true, inclusion: { in: %w(O-, O+, A-, A+, B-, B+, AB-, AB+), message: "%{value} no es un tipo de sangre válido. Los tipos de sangre válidos son: O-, O+, A-, A+, B-, B+, AB- y AB+" }
+  validates :email, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
